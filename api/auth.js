eca0aa6f5dfa1f621d641d4d6f60df1f5e023cfd
Alex Saunders <alex.saunders@formidable.com>
2020-02-08 15:46:56 +0000

    fix status

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
   const { access_token } = req.query;
-  if (access_token) {
-    res.status(400).send(`No access_token sent!`);
+  if (!access_token) {
+    return res.status(400).send(`No access_token sent!`);
   }
   res.send(`Hello ${req.query.access_token}!`);
 };
