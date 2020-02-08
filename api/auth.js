70fcf86effc7c6edc1a6b3322ac81a454db542b7
Alex Saunders <alex.saunders@formidable.com>
2020-02-08 16:05:15 +0000

    syntax error

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 const fetch = require('node-fetch');
 
 module.exports = (req, res) => {
@@ -10,12 +10,11 @@ module.exports = (req, res) => {
     return res.status(400).send(`No redirect_uri sent`);
   }
 
-  
   fetch('', {
     method: 'post',
     body: JSON.stringify({
-      client_id: process.env.client_id
-      client_secret: process.env.client_secret
+      client_id: process.env.client_id,
+      client_secret: process.env.client_secret,
       code: access_token,
       redirect_uri,
       state: 'glu'
@@ -23,5 +22,8 @@ module.exports = (req, res) => {
     headers: {
       'Content-Type': 'application/json'
     }
-  }).then((res) => res.text()).then((text) => res.status(200).send(text)).catch((err) => res.status(500).send(err));
+  })
+    .then(res => res.text())
+    .then(text => res.status(200).send(text))
+    .catch(err => res.status(500).send(err));
 };
