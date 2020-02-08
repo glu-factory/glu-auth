2152a2f2a5a33613833bcc93e4d969e289bf8ee9
Alex Saunders <alex.saunders@formidable.com>
2020-02-08 16:01:39 +0000

    add auth function

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,27 @@
+require('dotenv').config()
+const fetch = require('node-fetch');
+
 module.exports = (req, res) => {
-  const { access_token } = req.query;
+  const { access_token, redirect_uri } = req.query;
   if (!access_token) {
-    return res.status(400).send(`No access_token sent!`);
+    return res.status(400).send(`No access_token sent`);
   }
-  res.send(`Hello ${req.query.access_token}!`);
+  if (!redirect_uri) {
+    return res.status(400).send(`No redirect_uri sent`);
+  }
+
+  
+  fetch('', {
+    method: 'post',
+    body: JSON.stringify({
+      client_id: process.env.client_id
+      client_secret: process.env.client_secret
+      code: access_token,
+      redirect_uri,
+      state: 'glu'
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then((res) => res.text()).then((text) => res.status(200).send(text)).catch((err) => res.status(500).send(err));
 };
